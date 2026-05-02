016e64e458862d85cbad7f26c2dd4a2c7e218f96
Marcelo <marcelogvz27@gmail.com>
2026-05-02 02:58:19 -0300

    probando rutas

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,49 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
 //ruta base
 app.get("/", (req, res) => {
     res.send("servidor corriendo");
 });
 
+
+app.get("/products", (req, res) =>{
+    res.json(productos);
+});
+
+let productos = []
+app.post("/enviarProducto", (req, res) => {
+    productos.push(req.body);
+    res.send("enviar productos");
+
+})
+app.get("/admin", (req, res) => {
+    res.send("panel admin")
+});
+
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+
+    if (email === "marce@" && password === "1234"){
+        res.send("login exitoso");
+    } else {
+        res.send("datos incorrectos");
+    }
+});
+
+app.put("/actualizar", (req, res) => {
+    console.log(req.body);
+    res.send("actualizar datos");
+});
+
+
+
+// puerto en la cual escucha mi servidor 
 const PORT = 3000;
 
+
+//escuchar conexion 
 app.listen(PORT, () =>{
     console.log(`servidor corriendo en http://localhost:${PORT}`);
 });

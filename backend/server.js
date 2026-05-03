043a57f395c338531b521c2b9e1ba04d10d9fb0d
Marcelo <marcelogvz27@gmail.com>
2026-05-03 09:07:50 -0300

    servidor limpio

--- backend/server.js
+++ backend/server.js
@@ -17,71 +17,17 @@ mongoose.connect("mongodb+srv://marcelov:mongodb27@cluster0.wgdl93g.mongodb.net/
     .then(() => console.log("conectado a mongodb"))
     .catch(err => console.log(err));
 
-//modelo de productos (mongoDB)
-const Producto = require("./models/producto");
 
-//ruta base
+// rutas
+const productoRoutes = require("./routes/productoRoutes");
+app.use(productoRoutes);
+
+// base
 app.get("/", (req, res) => {
     res.send("servidor corriendo");
 });
 
-
-//ver productos
-app.get("/productos", async  (req, res) =>{
-    const productos = await Producto.find();
-    res.json(productos);
-});
-
-//admin panel
-app.get("/admin", async (req, res) => {
-    const productos = await Producto.find();
-    res.render("admin", {productos});
-})
-
-// Crear productos
-app.post("/crearProducto", async (req, res) => {
-    await Producto.create(req.body);
-    res.redirect("/admin");
-
-});
-
-//vista editar productos
-app.get("/editar/:id", async (req, res) => {
-    const producto = await Producto.findById(req.params.id,);
-    res.render("editar", {producto});
-});
-
-// generar edicion
-app.post("/editar/:id", async (req, res) => {
-    await Producto.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect("/admin")
-})
-
-//eliminar productos
-app.post("/eliminar/:id", async (req, res) => {
-    await Producto.findByIdAndDelete(
-        req.params.id, 
-        req.body);
-
-    res.redirect("/admin")
-});
-
-//datos login admin
-app.post("/login", (req, res) => {
-    const {email, password} = req.body;
-
-    if (email === "marce@" && password === "1234"){
-        res.send("login exitoso");
-    } else {
-        res.send("datos incorrectos");
-    }
-});
-
-// puerto en la cual escucha mi servidor 
-const PORT = 3000;
-
-
 //escuchar conexion 
-app.listen(PORT, () =>{
-    console.log(`servidor corriendo en http://localhost:${PORT}`);
+app.listen(3000, () =>{
+    console.log("servidor corriendo en http://localhost:3000");
 });

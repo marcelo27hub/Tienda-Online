66db44c9771df06b22836403dadf4fcc872f8814
Marcelo <marcelogvz27@gmail.com>
2026-05-03 09:50:50 -0300

    rutas admin

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 // iniciamos app server
 const app = express();
 
+//variable global, si el admin se logueo
+let isLoggedIn = false;
+
 // middleware para leeer JSON
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -20,12 +23,47 @@ mongoose.connect("mongodb+srv://marcelov:mongodb27@cluster0.wgdl93g.mongodb.net/
 
 // rutas
 const productoRoutes = require("./routes/productoRoutes");
+const producto = require("./models/producto");
 app.use(productoRoutes);
 
 // base
 app.get("/", (req, res) => {
     res.send("servidor corriendo");
 });
+
+//login
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// datos del admin
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === "marce@" && password === "1234") {
+        isLoggedIn = true
+        res.redirect("/admin");
+    } else {
+        res.send("Datos incorrectos");
+    }
+});
+
+// redireccionar a login si no se logueo. Y si lo hizo admin recibe los productos
+app.get("/admin", async (req, res) => {
+    if (!isLoggedIn) {
+        return res.redirect("/login");
+    }
+
+    const productos = await producto.find();
+    res.render("admin", { productos });
+});
+
+// logout 
+app.get("/logout", (req, res) => {
+    isLoggedIn = false;
+    res.redirect("/login")
+})
+
 
 //escuchar conexion 
 app.listen(3000, () =>{

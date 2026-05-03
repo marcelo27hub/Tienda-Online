127e9ea27f7de6e8f8bd009c94ec435eb97d98d5
Marcelo <marcelogvz27@gmail.com>
2026-05-03 19:42:24 -0300

    logica de admin

---
+++ backend/controllers/admincontroller.js
@@ -0,0 +1,23 @@
+//login
+exports.getLogin = (req, res) => {
+    res.render("login");
+};
+
+// enviar datos del admin
+exports.postLogin = (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === "marce@" && password === "1234") {
+        req.session.isLoggedIn = true;
+        return res.redirect("/admin");
+    }
+
+    res.send("Datos incorrectos");
+};
+
+// cerrar sesion
+exports.logout = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/login");
+    });
+};

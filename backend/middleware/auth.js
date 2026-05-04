d4ecb9d553bee6d06308a5628ea6bc179a49f8cb
Marcelo <marcelogvz27@gmail.com>
2026-05-03 22:22:39 -0300

    filtro para rutas privadas

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,5 @@
+// middleware para proteger rutas privadas
+//redirige al login si el usuario no esta autenticado
 module.exports = function (req, res, next) {
     if (!req.session.isLoggedIn) {
         return res.redirect("/login");

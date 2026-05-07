ab6ed645e2be3c2e8ab705bbbb15085b9847ae81
Marcelo <marcelogvz27@gmail.com>
2026-05-07 00:24:01 -0300

    logica de producto.js de mi backend para que puedan compartir la base ded datos

---
+++ frontend/models/producto.js
@@ -0,0 +1,13 @@
+//usamos mongoose para la base de datos
+const mongoose = require("mongoose");
+
+//modelo del producto
+const modelproduct = new mongoose.Schema({
+    imagen: { type: String },
+    nombre: {type: String, required: true, trim: true},
+    precio: {type: Number, required: true, min: 1},
+    descripcion: {type: String, required: true, trim: true}
+});
+
+//para poder usarlo en cualquier otro archivo
+module.exports = mongoose.model("producto", modelproduct);
